5574a791a45f6f5ebb9e9922eae052d4f8e34dd6
oxc <oxc>
2018-03-13 15:57:34 +0800

    完成购物车选中商品功能

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,41 @@
 import './cart_base.css'
 import './cart_trade.css'
 import './cart.css' 
+
+
+import Vue from 'vue'
+import mixin from 'js/mixin'
+import axios from 'js/axios'
+import api from 'js/api.js'
+
+
+new Vue({
+    el: '.container',
+    data: {
+        lists: null
+    },
+    methods: {
+        getList(){
+            axios.get(api.cartLists).then(res => {
+                let list =  res.data.cartList
+                list.forEach(e => {
+                    e.checked = true
+                    e.goodsList.forEach(v => {
+                        v.checked = true
+                    })
+                })
+                this.lists = list
+            })
+        },
+        selectGood(good){
+            good.checked = !good.checked
+        }
+    },
+    computed: {
+
+    },
+    created(){
+        this.getList()
+    },
+    mixins: [mixin]
+})
